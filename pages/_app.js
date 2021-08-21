230e801c984889ebf2be465b19b53b8375583d49
defunsm <salmanhossain500@gmail.com>
2021-08-20 22:28:11 -0400

    clerk login integration

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,19 @@ import Head from 'next/head'
 
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
+import { ClerkProvider, SignedIn, SignedUp, SignedOut, RedirectToSignIn } from '@clerk/nextjs'
+import { useRouter } from 'next/router'
+
+export const publicPages = []
 
 export default function App({ Component, pageProps }) {
+  // Get the pathname
+  const { pathname } = useRouter()
+  // Check if the current route matches a public page
+  const isPublicPage = publicPages.includes(pathname)
+
+  // If the current route is listed as public, render it directly
+  // Otherwise, use Clerk to require authentication
   return (
     <ThemeProvider attribute="class">
       <Head>
@@ -14,7 +25,20 @@ export default function App({ Component, pageProps }) {
       </Head>
       <Analytics />
       <LayoutWrapper>
-        <Component {...pageProps} />
+        <ClerkProvider>
+          {isPublicPage ? (
+            <Component {...pageProps} />
+          ) : (
+            <>
+              <SignedIn>
+                <Component {...pageProps} />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          )}
+        </ClerkProvider>
       </LayoutWrapper>
     </ThemeProvider>
   )
